68f857e967d8b9e7431acf40f147681035dc814e
palakSingh621 <kushwahaps621@gmail.com>
2024-09-10 11:07:05 +0530

    milestone 2

--- http-server/index.js
+++ http-server/index.js
@@ -1,3 +1,4 @@
+
 // ----------------File System & stream-----------------------------------------
 // const http=require("http");
 // const fs=require("fs");
@@ -26,7 +27,7 @@
 // console.log(arr[2]);
 // console.log(arr[3]);
 
-const minimist=require("minimist");
+// const minimist=require("minimist");
 
 // const arr=minimist(process.argv.slice(2));
 
@@ -47,11 +48,59 @@ const minimist=require("minimist");
 // }
 
 
-const args = minimist(process.argv.slice(2), {
-    alias: {
-      num1: 'n1',
-      num2: 'n2'
-    }
-  });
+// const args = minimist(process.argv.slice(2), {
+//     alias: {
+//       num1: 'n1',
+//       num2: 'n2'
+//     }
+//   });
   
-  console.log(`Num1 is: ${args.num1}`); 
+//   console.log(`Num1 is: ${args.num1}`);
+const minimist=require("minimist");
+const http=require("http");
+const fs=require("fs");
+
+let homeContent="";
+let projectContent="";
+let registerContent="";
+const port= minimist(process.argv.slice(2)).port;
+
+fs.readFile("home.html",(err,home)=>{
+  if(err){
+    throw err;
+  }
+  homeContent=home;
+});
+
+fs.readFile("project.html",(err,project)=>{
+  if(err){
+    throw err;
+  }
+  projectContent=project;
+});
+
+fs.readFile("registration.html",(err,register)=>{
+  if(err){
+    throw err;
+  }
+  registerContent=register;
+})
+console.log(port);
+http.createServer((req,res)=>{
+  const url=req.url;
+    res.writeHead(200,{"Content-Type": "text/html" });
+    switch(url){
+      case "/project":
+        res.write(projectContent);
+        res.end();
+        break;
+      case "/registration":
+        res.write(registerContent);
+        res.end();
+        break;
+      default:
+        res.write(homeContent);
+        res.end();
+        break;
+    } 
+  }).listen(port);
