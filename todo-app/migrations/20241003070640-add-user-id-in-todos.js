1c94c7a808110ee27bd86e4dd11128ee9a59fed7
palakSingh621 <kushwahaps621@gmail.com>
2024-10-05 13:59:20 +0530

    modifying add-user-id-in-todos

--- todo-app/migrations/20241003070640-add-user-id-in-todos.js
+++ todo-app/migrations/20241003070640-add-user-id-in-todos.js
@@ -3,21 +3,34 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    queryInterface.addColumn("Todos", "userId", {
-      type: Sequelize.DataTypes.INTEGER,
-    });
+    const tableDescription = await queryInterface.describeTable("Todos");
+    if (!tableDescription.userId) {
+      await queryInterface.addColumn("Todos", "userId", {
+        type: Sequelize.DataTypes.INTEGER,
+      });
+    }
 
-    queryInterface.addConstraint("Todos", {
-      fields: ["userId"],
-      type: "foreign key",
-      references: {
-        table: "Users",
-        field: "id",
-      },
-    });
+    const constraints = await queryInterface.showConstraint("Todos");
+    const constraintExists = constraints.some(
+      (constraint) =>
+        constraint.constraintName.includes("userId") &&
+        constraint.constraintType === "FOREIGN KEY",
+    );
+
+    if (!constraintExists) {
+      await queryInterface.addConstraint("Todos", {
+        fields: ["userId"],
+        type: "foreign key",
+        references: {
+          table: "Users",
+          field: "id",
+        },
+      });
+    }
   },
 
   async down(queryInterface, Sequelize) {
-    queryInterface.removeColumn("Todos", "userId");
+    await queryInterface.removeColumn("Todos", "userId");
+    await queryInterface.removeColumn("Todos", "userId");
   },
 };
